0f856b5d3c522801782d06c8bc43a2b52a31d93f
lcpojr <lcpojr@hotmail.com>
2017-09-04 10:33:52 -0300

    Sentry

--- aquariomaniacos/settings/prod.py
+++ aquariomaniacos/settings/prod.py
@@ -58,3 +58,60 @@ INSTALLED_APPS += ['gunicorn', 'raven.contrib.django.raven_compat',]
 #    ('django.template.loaders.cached.Loader', [
 #        'django.template.loaders.filesystem.Loader', 'django.template.loaders.app_directories.Loader', ]),
 #]
+
+import logging
+INSTALLED_APPS += ('raven.contrib.django.raven_compat', )
+SENTRY_DSN = 'https://aa74646645b74c89b6fa273115aaff2c:e19d00e996fd4751975e78c8d37ba06e@sentry.io/212407'
+SENTRY_CLIENT = 'raven.contrib.django.raven_compat.DjangoClient'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'django.security.DisallowedHost': {
+            'level': 'ERROR',
+            'handlers': ['console', 'sentry'],
+            'propagate': False,
+        },
+    },
+}
+SENTRY_CELERY_LOGLEVEL = logging.INFO
+RAVEN_CONFIG = {
+    'CELERY_LOGLEVEL': logging.INFO,
+    'DSN': SENTRY_DSN
+}

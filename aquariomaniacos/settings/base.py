5ad7ff889d73df47bd4a22d464ffc1088cf7ef1e
lcpojr <lcpojr@hotmail.com>
2017-09-04 17:20:28 -0300

    Envio de email

--- aquariomaniacos/settings/base.py
+++ aquariomaniacos/settings/base.py
@@ -160,3 +160,16 @@ MEDIA_URL = '/media/'
 
 # Login
 LOGIN_URL = 'painel:login'
+
+
+'''
+    EMAIL
+'''
+EMAIL_HOST = '174.138.37.216'
+EMAIL_HOST_USER = 'i3lcontato@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'i3lcontato@gmail.com'
+CONTACT_EMAILS = ['aquariomaniacos@bol.com.br']
